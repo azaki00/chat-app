f1a0b6887a0acd0e0c0ea6fa2924648c70a5962f
azaki00 <shadeaki162@gmail.com>
2024-01-11 16:10:09 +0200

    fixed ports

--- server/index.js
+++ server/index.js
@@ -10,7 +10,7 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
     cors: {
-        origin: "http://localhost:5173",
+        origin: "http://192.168.1.101:5173",
         methods: ["GET", "POST"],
         transports: ['websocket'],
     },
